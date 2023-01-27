e73d7ad140975265965eee187c75babfb16f4810
rararach-l <117470446+rararach-l@users.noreply.github.com>
2023-01-27 19:12:23 +0000

    working on display of forecast

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-const searchButton = document.querySelector("#search-button");
+const searchButton = document.querySelector(".search-button");
 const input = document.querySelector("#search-input");
 const apiKey = "fd607638c6f3eabf1629f69a02128177";
 
@@ -10,38 +10,58 @@ searchButton.addEventListener("click", function (event) {
         var placeLat = geoResponse.data[0].lat
         var placeLong = geoResponse.data[0].lon
         const queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + placeLat + "&lon=" + placeLong + "&appid=" + apiKey;
+
+        // check if the response is valid and alert to the user 
+        // add feature for country search?
         axios.get(queryURL).then(function (response) {
-            const cityName = "the weather in " + response.data.city.name + "" + response.data.city.country + " is: ";
+            // getting the data only (without time) using moment.js to convert unix
+            const timestamp = response.data.list[0].dt;
+            const dateConverted = moment.unix(timestamp).format("dddd, MMMM Do YYYY");
+
+            //adding the location information, with city and country
+            const locationDate = "the weather in " + response.data.city.name + ", " + response.data.city.country + " on " + dateConverted + " is:" ;
+
+            // description of the weather
             const description = response.data.list[0].weather[0].description + " ";
+
+            // windspeed
             const windSpeed = "with a windspeed of " + response.data.list[0].wind.speed + " mph ";
-            const date = "the local time is " + response.data.list[0].dt_txt;
-            const icon = response.data.list[0].weather[0].icon;
+
+            // the icon for the current weather
+            const icon = "http://openweathermap.org/img/w/" + response.data.list[0].weather[0].icon + ".png";
+
+            // the temperature
             const temp = "the temperature is " + ((response.data.list[0].main.temp - 273.15).toFixed(1)) + " centigrade";
-            const humidity = "the humidity is: " + response.data.list[0].main.humidity;
-            const weatherResult = cityName + description + windSpeed + date + icon + temp + humidity;
+
+            // the humidity
+            const humidity = "with humidity of " + response.data.list[0].main.humidity;
+
+            //console logging the whole report to check
+            const weatherResult = locationDate + description + windSpeed + icon + temp + humidity;
+
             console.log(weatherResult);
+            document.querySelector('.locationDate').innerHTML = locationDate;
+            document.querySelector('.description').innerHTML = description;
+            document.querySelector('.windSpeed').innerHTML = windSpeed;
+            document.querySelector('.humidity').innerHTML = humidity;
+            document.querySelector('.icon').src = icon;
+
+
 
             searchHistory();
 
             function searchHistory() {
                 var history = JSON.parse(localStorage.getItem("history")) || [];
-                localStorage.setItem("history", JSON.stringify(history));
-                localStorage.setItem("history", JSON.stringify(history));
                 var historySection = document.getElementById("history");
                 history.forEach(function (place) {
                     var button = document.createElement("button");
-                    button.className = "searchHistory";
+                    button.className = "search-button";
                     button.innerHTML = place;
                     historySection.appendChild(button);
                 });
-                const placeHistoryButtons = document.querySelectorAll(".searchHistory");
-                // placeHistoryButtons.forEach(function (button) {
-                //     button.addEventListener("click", function () {
-                //         // your event listener code here
-                //     });
-                // });
-
             }
         });
     });
 });
+
+// store the search as a key with the name London
